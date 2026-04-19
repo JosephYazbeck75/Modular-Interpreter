3aa8d5811ecc3f53bf51c6de704a58fbf818e68e
JosephYazbeck75 <josephyazbeck78@gmail.com>
2026-04-19 16:55:00 +0300

    Updated

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,7 +3,7 @@ public class Lexer {
     public int pos;
     private char curr;
 
-    public Lexer(String tex) {
+    public Lexer(String text) {
         this.text = text;
         this.pos = 0;
         this.curr = text.charAt(pos);
--- src/Type.java
+++ src/Type.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+public enum Type {
+    VAR,
+    NUMBER,
+    ADD,
+    MIN,
+    MULT,
+    DIV,
+    ASSIGN,
+    LEFTPAR,
+    RIGHTPAR,
+    SEMICOLON,
+    END,
+} 
