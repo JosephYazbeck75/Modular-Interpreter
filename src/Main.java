6a50a6ee5a90ebc9632696f8e1436ff17364fa93
JosephYazbeck75 <josephyazbeck78@gmail.com>
2026-04-28 15:15:07 +0300

    Updated Main
    Updated Main to test out the parser.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-Lexer lexer = new Lexer("a = 10 add 5;");
-
-Token token;
-    
-    do {
-        token = lexer.getNextToken();
-System.out.println(token);
-    } while (token.type != Type.END);
-}
+        Lexer lexer = new Lexer("a = 10 add 5; b = a min 3;");
+        Parser parser = new Parser(lexer);
+        ProgramNode program = parser.parseProgram();
+        System.out.println(program);
+    }
 }
